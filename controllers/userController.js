26ee864bd33f9b194351ef775b608bf46a92f9a5
tchoforwilson <tchoforwilson@gmail.com>
2023-03-21 14:13:36 +0100

    updated user controller filter and removed image upload functionality to a separate file

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,59 +1,7 @@
 'use strict';
-import multer from 'multer';
-import sharp from 'sharp';
 import AppError from '../utilities/appError.js';
 import catchAsync from '../utilities/catchAsync.js';
 import User from '../models/userModel.js';
-
-/**
- * @breif Multer memory storage
- */
-const multerStorage = multer.memoryStorage();
-
-/**
- * @breif Method to check if file uploaded is an image
- * @param {Request} req -> Request object
- * @param {File} file -> File field
- * @param {Callback} cb -> Callback function
- */
-const multerFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image')) {
-    cb(null, true);
-  } else {
-    cb(new AppError('Not an image! Please upload only images.', 400), false);
-  }
-};
-// Maximum size of user uploaded file(photo) 1MB
-const maxImageSize = 1 * 1000 * 1000;
-
-/**
- * @brief multer utility to upload image
- */
-const upload = multer({
-  storage: multerStorage,
-  limits: { fileSize: maxImageSize },
-  fileFilter: multerFilter,
-});
-
-export const uploadUserPhoto = upload.single('photo');
-
-/**
- * @breif Method to resize user photo, convert image to "jpeg and increase quality"
- */
-export const resizeUserPhoto = catchAsync(async (req, res, next) => {
-  if (!req.file) return next();
-
-  // Stored user image name string user-userId-currentDate.jpeg
-  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
-
-  await sharp(req.file.buffer)
-    .resize(500, 500)
-    .toFormat('jpeg')
-    .jpeg({ quality: 90 })
-    .toFile(`uploads/profiles/${req.file.filename}`);
-
-  next();
-});
 
 /**
  * @breif Filter out unwanted fields in an object
@@ -92,10 +40,14 @@ export const editProfile = catchAsync(async (req, res, next) => {
   if (req.file) filteredBody.photo = req.file.filename;
 
   // 3) Update user document
-  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
-    new: true,
-    runValidators: true,
-  });
+  const updatedUser = await User.findByIdAndUpdate(
+    req.member.id,
+    filteredBody,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
 
   res.status(200).json({
     status: 'success',
