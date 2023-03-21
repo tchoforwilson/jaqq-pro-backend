877b3be43d06cf7780421ce13864cfd925e10958
tchoforwilson <tchoforwilson@gmail.com>
2023-03-21 14:11:06 +0100

    provider routes

---
+++ routes/api/providerRoutes.js
@@ -0,0 +1,24 @@
+'use strict';
+import { Router } from 'express';
+import * as authController from '../../controllers/authentication/providerAuthController.js';
+import * as providerController from '../../controllers/providerController.js';
+import { uploadPhoto, resizePhoto } from '../../utilities/imageUpload.js';
+
+const router = Router();
+
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
+
+// Protect all routes after this middleware
+router.use(authController.protect);
+
+router.patch('/updatePassword', authController.updatePassword);
+router.patch(
+  '/editProfile',
+  uploadPhoto,
+  resizePhoto,
+  providerController.editProfile
+);
+
+export default router;
