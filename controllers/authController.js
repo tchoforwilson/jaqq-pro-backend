4e916c1495ed0659b4ee6375f2575d92a3106ee3
tchoforwilson <tchoforwilson@gmail.com>
2023-03-21 10:04:46 +0100

    added possibility for login with contact, email and password

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,13 +50,17 @@ export const login = catchAsync(async (req, res, next) => {
 
   // 1) Check if email and password exist
   if (!email || !password) {
-    return next(new AppError('Please provide email and password!', 400));
+    return next(
+      new AppError('Please provide email or contact and password!', 400)
+    );
   }
   // 2) Check if user exists && password is correct
-  const user = await User.findOne({ email }).select('+password');
+  const user = await User.findOne({
+    $or: [{ email }, { phone: email }],
+  }).select('+password');
 
   if (!user || !(await user.correctPassword(password, user.password))) {
-    return next(new AppError('Incorrect email or password', 401));
+    return next(new AppError('Incorrect email or contact or password', 401));
   }
 
   // 3) If everything ok, send token to client
