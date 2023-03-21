5af277a4116bda365991694c11d0f8c65763b4f6
tchoforwilson <tchoforwilson@gmail.com>
2023-03-21 11:40:03 +0100

    added logout route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -7,6 +7,7 @@ const router = Router();
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.get('/logout', authController.logout);
 
 // Protect all routes after this middleware
 router.use(authController.protect);
