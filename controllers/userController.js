8cb551d7eb784f7c2422021a35e886453c4cdc0e
tchoforwilson <tchoforwilson@gmail.com>
2023-03-23 06:54:36 +0100

    Refactored controllers to use middlewares from member controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,58 +1,20 @@
-'use strict';
-import AppError from '../utilities/appError.js';
-import catchAsync from '../utilities/catchAsync.js';
 import User from '../models/userModel.js';
+import * as memberController from './memberController.js';
 
-/**
- * @breif Filter out unwanted fields in an object
- * @param {Object} obj -> Provided object
- * @param  {...any} allowedFields -> Allowed fields array
- * @returns {Object}
- */
-const filterObj = (obj, ...allowedFields) => {
-  const newObj = {};
-  Object.keys(obj).forEach((el) => {
-    if (allowedFields.includes(el)) newObj[el] = obj[el];
-  });
-  return newObj;
-};
+export const { getMe } = memberController;
+export const updateMe = memberController.updateMe(
+  User,
+  'firstName',
+  'lastName',
+  'email',
+  'phone',
+  'dateOfBirth'
+);
+export const deleteMe = memberController.deleteMe(User);
 
-export const editProfile = catchAsync(async (req, res, next) => {
-  // 1) Create error if user POSTs password data
-  if (req.body.password || req.body.passwordConfirm) {
-    return next(
-      new AppError(
-        'This route is not for password updates. Please use /updatePassword.',
-        400
-      )
-    );
-  }
+export const createUser = memberController.createMember;
+export const getUser = memberController.getMember;
+export const getAllUsers = memberController.getAllMembers;
 
-  // 2) Filtered out unwanted fields names that are not allowed to be updated
-  const filteredBody = filterObj(
-    req.body,
-    'firstName',
-    'lastName',
-    'email',
-    'phone',
-    'dateOfBirth'
-  );
-  if (req.file) filteredBody.photo = req.file.filename;
-
-  // 3) Update user document
-  const updatedUser = await User.findByIdAndUpdate(
-    req.member.id,
-    filteredBody,
-    {
-      new: true,
-      runValidators: true,
-    }
-  );
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      user: updatedUser,
-    },
-  });
-});
+export const updateUser = memberController.updateMember;
+export const deleteUser = memberController.deleteMember;
