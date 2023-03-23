98dadf626d1befc12bb941661c36360f8e59f629
tchoforwilson <tchoforwilson@gmail.com>
2023-03-23 06:00:56 +0100

    added pre find schema for pricing provider and index to ensure pricing provider and task are unique

--- models/pricingModel.js
+++ models/pricingModel.js
@@ -3,14 +3,14 @@ import { Schema, model } from 'mongoose';
 const pricingSchema = new Schema(
   {
     provider: {
-      type: Schema.Types.ObjectId,
+      type: Schema.ObjectId,
       ref: 'Provider',
       required: [true, 'providers can set their minimum price for a task'],
     },
     task: {
-      type: Schema.Types.ObjectId,
+      type: Schema.ObjectId,
       ref: 'Task',
-      required: true,
+      required: [true, 'Pricing should have a task'],
     },
     minPrice: {
       type: Number,
@@ -23,6 +23,20 @@ const pricingSchema = new Schema(
   },
   { timestamps: true }
 );
+
+// NB: This is to make sure a provider sets only one price for a task
+pricingSchema.index({ provider: 1, task: 1 }, { unique: true });
+
+/**
+ * @breif When finding a pricing, add provider to the search result
+ */
+pricingSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'provider',
+    select: 'firstName lastName photo phone',
+  });
+  next();
+});
 
 const Pricing = model('Pricing', pricingSchema);
 
