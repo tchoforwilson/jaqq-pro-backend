63f9ddbd903f95ba2ce90887b132458c43712778
tchoforwilson@gmail.com <wilson-tchofor@idealbox.org>
2023-04-27 09:02:14 +0100

    added controllers for resendCode, verifyMe and updateContact

--- controllers/authentication/providerAuthController.js
+++ controllers/authentication/providerAuthController.js
@@ -2,9 +2,12 @@ import Provider from '../../models/providerModel.js';
 import authController from './authController.js';
 
 export default {
+  resendCode: authController.resendcode(Provider),
   register: authController.register(Provider),
   login: authController.login(Provider),
+  verifyMe: authController.verifyMe(Provider),
   logout: authController.logout,
   protect: authController.protect(Provider),
+  updateContact: authController.updateContact(Provider),
   updatePassword: authController.updatePassword(Provider),
 };
