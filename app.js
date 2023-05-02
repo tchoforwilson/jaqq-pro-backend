eada5962c7eaf5124e6d35bcbdb225eb7c957bce
tchoforwilson@gmail.com <wilson-tchofor@idealbox.org>
2023-05-02 19:42:11 +0100

    refactored environmental variables

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-import express, { json, urlencoded } from 'express';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import { config } from 'dotenv';
-import { fileURLToPath } from 'url';
+import express, { json, urlencoded } from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import cookieParser from "cookie-parser";
+import { fileURLToPath } from "url";
 
-import globalErrorHandler from './controllers/errorController.js';
-import AppError from './utilities/appError.js';
-import userRouter from './routes/api/userRoutes.js';
-import providerRouter from './routes/api/providerRoutes.js';
-import taskRouter from './routes/api/taskRoutes.js';
-import pricingRouter from './routes/api/pricingRoutes.js';
-
-config({ path: './config.env' });
+import config from "./configurations/config.js";
+import globalErrorHandler from "./controllers/errorController.js";
+import AppError from "./utilities/appError.js";
+import userRouter from "./routes/userRoutes.js";
+import providerRouter from "./routes/providerRoutes.js";
+import taskRouter from "./routes/taskRoutes.js";
+import pricingRouter from "./routes/pricingRoutes.js";
 
 // Start express app
 const app = express();
@@ -24,16 +22,16 @@ app.use(helmet());
 
 // Serving static files
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Development logging
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (config.env === "development") {
+  app.use(morgan("dev"));
 }
 
 // Body parser, reading data from body into req.body
-app.use(json({ limit: '10kb' }));
-app.use(urlencoded({ extended: true, limit: '10kb' }));
+app.use(json({ limit: "10kb" }));
+app.use(urlencoded({ extended: true, limit: "10kb" }));
 app.use(cookieParser());
 
 // Test middleware
@@ -43,13 +41,13 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/providers', providerRouter);
-app.use('/api/v1/tasks', taskRouter);
-app.use('/api/v1/pricings', pricingRouter);
+app.use(`${config.prefix}/users`, userRouter);
+app.use(`${config.prefix}/providers`, providerRouter);
+app.use(`${config.prefix}/tasks`, taskRouter);
+app.use(`${config.prefix}/pricings`, pricingRouter);
 
 // INVALID ROUTES
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
