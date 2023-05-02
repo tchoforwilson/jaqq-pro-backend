b7fd54d5f0c90959ccaea7cfe9fcd8ad08267589
tchoforwilson@gmail.com <wilson-tchofor@idealbox.org>
2023-05-02 19:38:55 +0100

    remove api folder and functions folder and refactored imported controller directory

---
+++ routes/userRoutes.js
@@ -0,0 +1,36 @@
+import { Router } from "express";
+import authController from "../controllers/authentication/userAuthController.js";
+import userController from "../controllers/userController.js";
+import { uploadPhoto, resizePhoto } from "../utilities/imageUpload.js";
+
+const router = Router();
+
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.get("/logout", authController.logout);
+
+// Protect all routes after this middleware
+router.use(authController.protect);
+
+router.patch("/updatePassword", authController.updatePassword);
+router.patch("/updateContact", authController.updateContact);
+router.get("/profile", userController.getMe, userController.getUser);
+router.patch("/editProfile", uploadPhoto, resizePhoto, userController.updateMe);
+router.delete("/deleteProfile", userController.deleteMe);
+router.post("/verifyMe", authController.verifyMe);
+router.get("/resendCode", authController.resendCode);
+
+// TODO: This routes below should be restricted to an admin user
+
+router
+  .route("/")
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route("/:id")
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+export default router;
