a3167af592d82aeac2328712b40ba51058f0ccf5
tchoforwilson@gmail.com <wilson-tchofor@idealbox.org>
2023-08-08 21:09:21 +0100

    http status code enumerations

---
+++ utilities/enums/e.status-code.js
@@ -0,0 +1,13 @@
+/**
+ * @breif Reponse status for http requests
+ */
+export default {
+  BAD_REQUEST: 400,
+  FORBIDDEN: 401,
+  UN_AUTHORIZED: 403,
+  NOT_FOUND: 404,
+  CREATED: 201,
+  SUCCESS: 200,
+  NO_CONTENT: 204,
+  SERVER_ERROR: 500,
+};
