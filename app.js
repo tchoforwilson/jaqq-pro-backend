902859abfd6b283059476407b7b95efed906aec4
tchoforwilson <tchoforwilson@gmail.com>
2023-11-15 08:44:37 +0100

    Added authentication route

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-import express, { json, urlencoded } from 'express';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import { fileURLToPath } from 'url';
+import express, { json, urlencoded } from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import cookieParser from "cookie-parser";
+import { fileURLToPath } from "url";
 
-import config from './configurations/config.js';
-import globalErrorHandler from './controllers/error.controller.js';
-import AppError from './utilities/appError.js';
-import userRouter from './routes/user.routes.js';
-import taskRouter from './routes/task.routes.js';
-import pricingRouter from './routes/pricing.routes.js';
+import config from "./configurations/config.js";
+import globalErrorHandler from "./controllers/error.controller.js";
+import AppError from "./utilities/appError.js";
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import taskRouter from "./routes/task.routes.js";
+import pricingRouter from "./routes/pricing.routes.js";
 
 // Start express app
 const app = express();
@@ -21,16 +22,16 @@ app.use(helmet());
 
 // Serving static files
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Development logging
-if (config.env === 'development') {
-  app.use(morgan('dev'));
+if (config.env === "development") {
+  app.use(morgan("dev"));
 }
 
 // Body parser, reading data from body into req.body
-app.use(json({ limit: '10kb' }));
-app.use(urlencoded({ extended: true, limit: '10kb' }));
+app.use(json({ limit: "10kb" }));
+app.use(urlencoded({ extended: true, limit: "10kb" }));
 app.use(cookieParser());
 
 // Test middleware
@@ -40,12 +41,13 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
+app.use(`${config.prefix}/auth`, authRouter);
 app.use(`${config.prefix}/users`, userRouter);
 app.use(`${config.prefix}/tasks`, taskRouter);
 app.use(`${config.prefix}/pricings`, pricingRouter);
 
 // INVALID ROUTES
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
