4bf27775855f7bb0020ec62cb890ae6ad9fa9cf5
tchoforwilson <tchoforwilson@gmail.com>
2023-11-15 19:39:06 +0100

    Removed all routes which requires authentication to auth.routes.js form user.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,4 +6,11 @@ const router = Router();
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 
+router.use(authController.protect);
+
+router.patch("/update-my-password", authController.updatePassword);
+router.patch("/update-my-phone", authController.updatePhone);
+router.post("/verify-sms-code", authController.verifySMSCode);
+router.get("/resend-sms-code", authController.resendSMSCode);
+
 export default router;
