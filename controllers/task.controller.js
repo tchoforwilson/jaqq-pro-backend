9f1951ed2a96ae1ea1c9f0e49c384a4defcd7848
tchoforwilson <tchoforwilson@gmail.com>
2023-11-17 06:59:41 +0100

    Implemented controller for setting user id on create task

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,11 +1,21 @@
-import factory from './handler.factory.js';
-import Task from '../models/task.model.js';
+import factory from "./handler.factory.js";
+import Task from "../models/task.model.js";
+
+/**
+ * @breif Set user id when creating a new task
+ * if it doesn't exist
+ */
+const setTaskUserIds = (req, res, next) => {
+  if (req.body.userId) req.body.userId = req.user.id;
+  next();
+};
 
 export default {
+  setTaskUserIds,
   createTask: factory.createOne(Task),
   getTask: factory.getOne(Task, {
-    path: 'providers',
-    select: '-__v -passwordChangedAt',
+    path: "users",
+    select: "-__v -passwordChangedAt",
   }),
   getAllTasks: factory.getAll(Task),
   updateTask: factory.updateOne(Task),
