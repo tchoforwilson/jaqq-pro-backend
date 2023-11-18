6fa28458da481a42ba489b21a1aa6fbf1bdc2934
tchoforwilson <tchoforwilson@gmail.com>
2023-11-18 10:41:30 +0100

    Added count routes

--- routes/pricing.routes.js
+++ routes/pricing.routes.js
@@ -1,18 +1,20 @@
-import { Router } from 'express';
-import pricingController from '../controllers/pricing.controller.js';
-import authController from '../controllers/auth.controller.js';
+import { Router } from "express";
+import pricingController from "../controllers/pricing.controller.js";
+import authController from "../controllers/auth.controller.js";
 
 const router = Router({ mergeParams: true });
 
 router.use(authController.protect);
 
+router.get("/count", pricingController.countPricing);
+
 router
-  .route('/')
+  .route("/")
   .get(pricingController.getAllPricings)
   .post(pricingController.setProviderTaskIds, pricingController.createPricing);
 
 router
-  .route('/:id')
+  .route("/:id")
   .get(pricingController.getPricing)
   .patch(pricingController.updatePricing)
   .delete(pricingController.deletePricing);
