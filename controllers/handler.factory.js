6a935039b83e687e2ec346fd0cc6980eb3ea5c84
tchoforwilson <tchoforwilson@gmail.com>
2023-11-18 10:15:22 +0100

    Handler factory for all CRUD Count and Search Operations reimplemented

--- controllers/handler.factory.js
+++ controllers/handler.factory.js
@@ -1,6 +1,7 @@
 import APIFeatures from "../utilities/apiFeatures.js";
 import AppError from "../utilities/appError.js";
 import catchAsync from "../utilities/catchAsync.js";
+import eStatusCode from "../utilities/enums/e.status-code.js";
 
 /**
  * @breif Create a new document in a database collection
@@ -9,10 +10,13 @@ import catchAsync from "../utilities/catchAsync.js";
  */
 const createOne = (Model) =>
   catchAsync(async (req, res, next) => {
+    // 1. Create a new item
     const doc = await Model.create(req.body);
 
+    // 2. Send response
     res.status(eStatusCode.CREATED).json({
       status: "success",
+      message: "Successfully created",
       data: doc,
     });
   });
@@ -26,19 +30,25 @@ const createOne = (Model) =>
  */
 const getOne = (Model, popOptions) =>
   catchAsync(async (req, res, next) => {
+    // 1. Get doc by id
     let query = await Model.findById(req.params.id);
 
+    // 2. Populate
     if (popOptions) query = query.populate(popOptions);
 
+    // 3. Perform query
     const doc = await query;
 
+    // 4. Check if item exists
     if (!doc)
       return next(
-        new AppError("No document found with that ID!", eStatusCode.NOT_FOUND)
+        new AppError("document not found with that ID!", eStatusCode.NOT_FOUND)
       );
 
+    // 5. Send response
     res.status(eStatusCode.SUCCESS).json({
       status: "success",
+      message: "Data receive successfully",
       data: doc,
     });
   });
@@ -51,18 +61,22 @@ const getOne = (Model, popOptions) =>
  */
 const updateOne = (Model) =>
   catchAsync(async (req, res, next) => {
+    // 1. Get item by id and update
     const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true,
     });
 
+    // 2. Check if item exists
     if (!doc)
       return next(
         new AppError("No document found with that ID!", eStatusCode.NOT_FOUND)
       );
 
+    // 3. Send response
     res.status(eStatusCode.SUCCESS).json({
       status: "success",
+      message: "Successfully updated",
       data: doc,
     });
   });
@@ -75,48 +89,106 @@ const updateOne = (Model) =>
  */
 const getAll = (Model) =>
   catchAsync(async (req, res, next) => {
-    // To allow for nested GET tasks & provider on pricing
+    // 1. To allow for nested GET routes
     let filter = {};
-    if (req.params.taskId) filter = { task: req.params.taskId };
-    if (req.params.providerId) filter = { provider: req.params.providerId };
 
-    // EXECUTE THE QUERY
+    if (req.params.userId) filter = { user: req.params.userId };
+    if (req.params.taskId) filter = { task: req.params.taskId };
+    if (req.params.pricingId) filter = { pricing: req.params.pricingId };
+
+    // 2. Build api query
     const features = new APIFeatures(Model.find(filter), req.query)
       .filter()
       .sort()
       .limitFields()
       .paginate();
 
+    // 3. EXECUTE THE QUERY
     const docs = await features.query;
 
-    // SEND RESPONSE
+    // 4. SEND RESPONSE
     res.status(eStatusCode.SUCCESS).json({
       status: "success",
       results: docs.length,
-      data: {
-        data: docs,
-      },
+      data: docs,
     });
   });
 
 /**
  * @breif Delete a single document in the database collection
  * @param {Collection} Model -> Database collection
- * @returns {Function}
+ * @returns function
  */
 const deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
+    // 1. Get item
     const doc = await Model.findByIdAndDelete(req.params.id);
 
+    // 2. Check if item exists
     if (!doc) {
       return next(
         new AppError("No document found with that ID", eStatusCode.NOT_FOUND)
       );
     }
 
-    res.status(204).json({
+    // 3. Send response
+    res.status(eStatusCode.NO_CONTENT).json({
       status: "success",
+      message: "Successfully deleted",
       data: null,
+    });
+  });
+
+/**
+ * @breif Search for documents marching request name
+ * @param {Collection} Model Database collection/model
+ * @returns {function}
+ */
+const search = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // 1. Get the query
+    const { q } = req.query;
+
+    // 2. Get the results
+    const results = await Model.find({
+      $or: [
+        { firstName: { $regex: q, $options: "i" } },
+        { lastName: { $regex: q, $options: "i" } },
+        { phone: { $regex: q, $options: "i" } },
+        { label: { $regex: q, $options: "i" } },
+      ],
+    });
+
+    // 3. Send the response
+    res.status(200).json({
+      status: "success",
+      data: results,
+    });
+  });
+
+/**
+ * @brief Count the number of document in a collection
+ * @param {Collection} Model  Database model/collection
+ * @returns {Function}
+ */
+const count = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // 1. Build filter
+    let filtered = {};
+    if (req.params.userId) filtered.category = req.params.userId;
+    if (req.params.taskId) filtered.store = req.params.taskId;
+    if (req.params.pricingId) filtered.product = req.params.pricingId;
+
+    // 2. Create search query
+    const searchQuery = { ...filtered, ...req.query };
+
+    // 3. Execute query
+    const count = await Model.count(searchQuery);
+
+    // 4. Send response
+    res.status(200).json({
+      status: "success",
+      data: count,
     });
   });
 
@@ -126,4 +198,6 @@ export default {
   updateOne,
   getAll,
   deleteOne,
+  search,
+  count,
 };
