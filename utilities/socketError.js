64b03db4e0838d9e28861e7fe459a36cd27f0524
tchoforwilson <tchoforwilson@gmail.com>
2023-11-22 05:57:24 +0100

    Handle sockets error

---
+++ utilities/socketError.js
@@ -0,0 +1,14 @@
+class SocketError extends Error {
+  /**
+   * @breif SocketError class constructor
+   * @param {String} message Error message
+   * @param {Number} code Error code
+   */
+  constructor(message, code) {
+    super(message);
+    this.code = code;
+    this.name = "SocketError";
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
