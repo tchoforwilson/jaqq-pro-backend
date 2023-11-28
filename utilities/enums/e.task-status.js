9ad0bc3939778318db581fd1a3286f60087c353f
tchoforwilson <tchoforwilson@gmail.com>
2023-11-28 06:30:30 +0100

    Changed tasks status

---
+++ utilities/enums/e.task-status.js
@@ -0,0 +1,10 @@
+/**
+ * @breif Status of various tasks
+ */
+export default {
+  PENDING: "Pending",
+  ACCEPTED: "Accepted",
+  PROGRESS: "In progress",
+  completed: "Completed",
+  cancelled: "Cancelled",
+};
