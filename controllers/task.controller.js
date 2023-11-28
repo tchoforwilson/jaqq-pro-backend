50ed82f1b4df3c189793af54a5580a523ec3886e
tchoforwilson <tchoforwilson@gmail.com>
2023-11-28 06:29:27 +0100

    Began implementing controllers for tasks

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,5 +1,57 @@
-import factory from "./handler.factory.js";
+import jwt from "jsonwebtoken";
+import { promisify } from "util";
 import Task from "../models/task.model.js";
+import catchSocket from "../utilities/catchSocket.js";
+import SocketError from "../utilities/socketError.js";
+
+const socketProtect = catchSocket(async (socket, next) => {
+  // 1. Get token
+  let token;
+  if (
+    socket.handshake.authorization.token &&
+    socket.handshake.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return next(
+      new SocketError(
+        "The user belonging to this token does no longer exist.",
+        400
+      )
+    );
+  }
+
+  // 2) Verification token
+  const decoded = await promisify(jwt.verify)(token, config.jwt.secret);
+
+  // 3) Check if user still exists
+  const currentuser = await User.findById(decoded.user._id);
+
+  if (!currentuser) {
+    return next(
+      new SocketError(
+        "The user belonging to this token does no longer exist.",
+        eStatusCode.FORBIDDEN
+      )
+    );
+  }
+
+  // 4) Check if user changed password after the token was issued
+  if (currentuser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new SocketError(
+        "User recently changed password! Please log in again.",
+        eStatusCode.FORBIDDEN
+      )
+    );
+  }
+
+  // GRANT ACCESS TO PROTECTED ROUTE
+  socket.user = currentuser;
+  next();
+});
 
 /**
  * @breif Set user id when creating a new task
@@ -10,16 +62,16 @@ const setTaskUserIds = (req, res, next) => {
   next();
 };
 
-export default {
-  setTaskUserIds,
-  createTask: factory.createOne(Task),
-  getTask: factory.getOne(Task, {
-    path: "users",
-    select: "-__v -passwordChangedAt",
-  }),
-  getAllTasks: factory.getAll(Task),
-  updateTask: factory.updateOne(Task),
-  deleteTask: factory.deleteOne(Task),
-  countTasks: factory.count(Task),
-  searchTask: factory.search(Task),
-};
+// export default {
+//   setTaskUserIds,
+//   createTask: factory.createOne(Task),
+//   getTask: factory.getOne(Task, {
+//     path: "users",
+//     select: "-__v -passwordChangedAt",
+//   }),
+//   getAllTasks: factory.getAll(Task),
+//   updateTask: factory.updateOne(Task),
+//   deleteTask: factory.deleteOne(Task),
+//   countTasks: factory.count(Task),
+//   searchTask: factory.search(Task),
+// };
