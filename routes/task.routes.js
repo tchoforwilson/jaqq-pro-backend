7f4ce10ee336a762a34aec1cf09bd9028d4757c4
tchoforwilson <tchoforwilson@gmail.com>
2023-11-28 06:29:49 +0100

    Commented task http routes

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,36 +1,36 @@
-import { Router } from "express";
-import pricingRouter from "./pricing.routes.js";
-import authController from "../controllers/auth.controller.js";
-import taskController from "../controllers/task.controller.js";
-import eUserRole from "../utilities/enums/e.user-role.js";
+// import { Router } from "express";
+// import pricingRouter from "./pricing.routes.js";
+// import authController from "../controllers/auth.controller.js";
+// import taskController from "../controllers/task.controller.js";
+// import eUserRole from "../utilities/enums/e.user-role.js";
 
-const router = Router({ mergeParams: true });
+// const router = Router({ mergeParams: true });
 
-router.use(authController.protect);
+// router.use(authController.protect);
 
-router.use("/:taskId/pricings", pricingRouter);
+// router.use("/:taskId/pricings", pricingRouter);
 
-router.get("/search", taskController.searchTask);
-router.get("/count", taskController.countTasks);
+// router.get("/search", taskController.searchTask);
+// router.get("/count", taskController.countTasks);
 
-router
-  .route("/")
-  .get(taskController.getAllTasks)
-  .post(
-    authController.restrictTo(eUserRole.PROVIDER),
-    taskController.setTaskUserIds,
-    taskController.createTask
-  );
-router
-  .route("/:id")
-  .get(taskController.getTask)
-  .patch(
-    authController.restrictTo(eUserRole.PROVIDER),
-    taskController.updateTask
-  )
-  .delete(
-    authController.restrictTo(eUserRole.PROVIDER),
-    taskController.deleteTask
-  );
+// router
+//   .route("/")
+//   .get(taskController.getAllTasks)
+//   .post(
+//     authController.restrictTo(eUserRole.PROVIDER),
+//     taskController.setTaskUserIds,
+//     taskController.createTask
+//   );
+// router
+//   .route("/:id")
+//   .get(taskController.getTask)
+//   .patch(
+//     authController.restrictTo(eUserRole.PROVIDER),
+//     taskController.updateTask
+//   )
+//   .delete(
+//     authController.restrictTo(eUserRole.PROVIDER),
+//     taskController.deleteTask
+//   );
 
-export default router;
+// export default router;
