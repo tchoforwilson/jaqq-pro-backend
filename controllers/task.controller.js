68019030dc7e776096597182fbc25007452aba57
tchoforwilson <tchoforwilson@gmail.com>
2023-11-29 04:38:24 +0100

    Task controller

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,12 +1,16 @@
 import jwt from "jsonwebtoken";
 import { promisify } from "util";
 import Task from "../models/task.model.js";
+import User from "../models/user.model.js";
+import APIFeatures from "../utilities/apiFeatures.js";
 import catchSocket from "../utilities/catchSocket.js";
 import SocketError from "../utilities/socketError.js";
+import eTaskStatus from "../utilities/enums/e.task-status.js";
 
-const socketProtect = catchSocket(async (socket, next) => {
+const protect = catchSocket(async (socket, next) => {
   // 1. Get token
   let token;
+  console.log(socket);
   if (
     socket.handshake.authorization.token &&
     socket.handshake.authorization.startsWith("Bearer")
@@ -17,7 +21,7 @@ const socketProtect = catchSocket(async (socket, next) => {
   if (!token) {
     return next(
       new SocketError(
-        "The user belonging to this token does no longer exist.",
+        "You are not logged in! Please log in to get access.",
         400
       )
     );
@@ -54,24 +58,82 @@ const socketProtect = catchSocket(async (socket, next) => {
 });
 
 /**
+ * @breif Middleware to restrict route access only to user of
+ * a particular role
+ * @param  {...any} roles -> User roles
+ * @returns {Function}
+ */
+const restrictTo = (...roles) => {
+  return (socket, next) => {
+    // roles ['admin', 'user','provider']. role='user'
+    if (!roles.includes(socket.user.role)) {
+      return next(
+        new SocketError(
+          "You do not have permission to perform this action",
+          403
+        )
+      );
+    }
+
+    next();
+  };
+};
+
+/**
  * @breif Set user id when creating a new task
  * if it doesn't exist
  */
-const setTaskUserIds = (req, res, next) => {
-  if (req.body.userId) req.body.userId = req.user.id;
+const setTaskUser = (socket, data, next) => {
+  if (!data.user) data.user = socket.user;
   next();
 };
 
-// export default {
-//   setTaskUserIds,
-//   createTask: factory.createOne(Task),
-//   getTask: factory.getOne(Task, {
-//     path: "users",
-//     select: "-__v -passwordChangedAt",
-//   }),
-//   getAllTasks: factory.getAll(Task),
-//   updateTask: factory.updateOne(Task),
-//   deleteTask: factory.deleteOne(Task),
-//   countTasks: factory.count(Task),
-//   searchTask: factory.search(Task),
-// };
+const createTask = catchSocket(async (socket, data) => {
+  console.log(data);
+  // 1. Create a new task
+  const newTask = await Task.create(data);
+
+  // 2. Find providers who are online and their location is close to the task's location
+  const providers = await User.find({
+    role: "provider",
+    online: true,
+    location: { $near: [newTask.location] },
+  });
+
+  // 3. Assign the task to a provider if found
+  if (providers.length > 0) {
+    newTask.assignee = providers[0].id;
+    newTask.status = eTaskStatus.ASSIGNED;
+
+    await newTask.save();
+
+    socket.emit("Task:Assigned", newTask);
+  }
+
+  socket.emit("Task:Created", newTask);
+});
+
+const getAllTasks = catchSocket(async (socket) => {
+  // 1. For nested query
+
+  // 2. Build query
+  const features = new APIFeatures(Task.find(filter), socket.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  // 3. EXECUTE THE QUERY
+  const docs = await features.query;
+
+  // 4. Emit response
+  socket.emit("Tasks:GetAll", docs);
+});
+
+export default {
+  protect,
+  restrictTo,
+  setTaskUser,
+  createTask,
+  getAllTasks,
+};
