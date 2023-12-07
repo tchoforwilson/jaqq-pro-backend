10ac7c985d0f4938719271d146421d44d1a6c161
tchoforwilson <tchoforwilson@gmail.com>
2023-12-07 05:35:17 +0100

    Separate route for user profile image update

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
 import authController from "../controllers/auth.controller.js";
 import userController from "../controllers/user.controller.js";
-// import taskRouter from "./task.routes.js";
-import { uploadPhoto, resizePhoto } from "../utilities/imageUpload.js";
+import taskRouter from "./task.routes.js";
 import eUserRole from "../utilities/enums/e.user-role.js";
 
 const router = Router();
@@ -11,14 +10,20 @@ const router = Router();
 router.use(authController.protect);
 
 // Fetching and creating provider tasks
-// router.use("/:userId/taks", taskRouter);
+router.use("/:userId/taks", taskRouter);
 
 router.route("/search", userController.searchUser);
 router.route("/count", userController.countUsers);
 
 // User routes
 router.get("/me", userController.getMe, userController.getUser);
-router.patch("/update-me", uploadPhoto, resizePhoto, userController.updateMe);
+router.patch("/update-me", userController.updateMe);
+router.patch(
+  "/update-photo",
+  userController.uploadUserPhoto,
+  userController.resizeUserPhoto,
+  userController.updateMe
+);
 router.delete("/delete-me", userController.deleteMe);
 
 router.use(authController.restrictTo(eUserRole.ADMIN));
