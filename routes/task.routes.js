b7cd13063d6a88f7f3e459e33eb3f5d2f0c83350
tchoforwilson <tchoforwilson@gmail.com>
2024-01-09 20:55:22 +0100

    Route for admin or user to delete task

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,20 +1,20 @@
-import { Router } from "express";
-import authController from "../controllers/auth.controller.js";
-import taskController from "../controllers/task.controller.js";
-import eUserRole from "../utilities/enums/e.user-role.js";
+import { Router } from 'express';
+import authController from '../controllers/auth.controller.js';
+import taskController from '../controllers/task.controller.js';
+import eUserRole from '../utilities/enums/e.user-role.js';
 
 const router = Router({ mergeParams: true });
 
 router.use(authController.protect);
 
 router.patch(
-  "/:taskId/set-status",
+  '/:taskId/set-status',
   authController.restrictTo(eUserRole.PROVIDER, eUserRole.USER),
   taskController.toggleTaskStatus
 );
 
 router
-  .route("/")
+  .route('/')
   .post(
     authController.restrictTo(eUserRole.USER),
     taskController.setTaskUserId,
@@ -23,13 +23,16 @@ router
   .get(taskController.getAllTasks);
 
 router
-  .route("/:id")
+  .route('/:id')
   .get(taskController.getTask)
   .patch(
     authController.restrictTo(eUserRole.USER),
     taskController.setTaskUserId,
     taskController.updateTask
   )
-  .delete(taskController.deleteTask);
+  .delete(
+    authController.restrictTo(eUserRole.ADMIN, eUserRole.USER),
+    taskController.deleteTask
+  );
 
 export default router;
