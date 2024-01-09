c255f49bb74fca08bfcb115169bf0b9fca633ccd
tchoforwilson <tchoforwilson@gmail.com>
2024-01-09 22:06:05 +0100

    Refactored toggle task status for various status

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -5,6 +5,7 @@ import User from '../models/user.model.js';
 import Task from '../models/task.model.js';
 import eUserRole from '../utilities/enums/e.user-role.js';
 import eTaskStatus from '../utilities/enums/e.task-status.js';
+import handleAsync from '../utilities/errors/handleAsync.js';
 import catchAsync from '../utilities/catchAsync.js';
 import AppError from '../utilities/appError.js';
 import {
@@ -12,38 +13,41 @@ import {
   CONST_ZEROU,
   MAX_PROVIDER_DISTANCE,
 } from '../utilities/constants/index.js';
-import { response } from 'express';
 
+/**
+ * @breif Set task user id
+ */
 const setTaskUserId = (req, res, next) => {
   if (!req.body.user) req.body.user = req.user.id;
   next();
 };
 
-const reassignTasks = async () => {
+/**
+ * @breif Reassign all pending tasks
+ */
+const reassignTasks = handleAsync(async () => {
   // 1. Get all taskes with pending status
   const tasks = await Task.find({ status: eTaskStatus.PENDING });
 
-  // 2. If there are any tasks, reassign them
+  // 2. If there are any tasks, assign them
   if (tasks.length > CONST_ZEROU) {
     for (const task of tasks) {
       await assignedTaskToProvider(task);
-      io.emit('task:reassign', {
-        data: task,
-        message: `Task ${task.service.label} reassigned to user ${task.user.firstName}`,
-      });
     }
   }
-};
+});
 
 // Set a timeout to check for provider response
-let tasksToReassign = [];
 cron.schedule(
   '*/2 * * * *',
-  catchAsync(async () => {
-    // 1. Get all assigned tasks
+  handleAsync(async () => {
+    // 1. Assign all pending tasks
+    reassignTasks();
+
+    // 2. Get all assigned tasks
     const assignedTasks = await Task.find({ status: eTaskStatus.ASSIGNED });
 
-    // 2. Search for tasks assigned to providers
+    // 3. Search for tasks assigned to providers
     for (const assignTask of assignedTasks) {
       const assignedProvider = await User.findOne({ _id: assignTask.provider });
       if (
@@ -51,25 +55,20 @@ cron.schedule(
         !assignedProvider.online ||
         assignedProvider.role !== eUserRole.PROVIDER
       ) {
-        // 3. Update task status
-        assignTask.status = eTaskStatus.PENDING;
-        await Task.updateOne(
-          { _id: assignTask.id },
-          { provider: null, status: eTaskStatus.PENDING }
-        );
+        // await assignTask.save({ validateBeforeSave: false }); TODO: Check if this works properly
+        // 4. Update task status
+        const unAssignTask = await Task.findByIdAndUpdate(assignTask.id, {
+          provider: null,
+          status: eTaskStatus.PENDING,
+        });
 
-        // 4. Emit response to user about task status
+        // 5. Emit response to user about task status
         io.emit('task:unassigned', {
-          data: assignTask,
+          data: unAssignTask,
           message: `Task ${assignTask.service.label} status updated to unassigned, as provider didn't respond within 5 minutes.`,
         });
-        tasksToReassign.push(assignTask);
       }
-      return;
     }
-
-    // 5. Reassign task to new provider
-    await reassignTasks();
   })
 );
 
@@ -77,9 +76,9 @@ cron.schedule(
  * @breif Assigntask to provider who is online
  * @param {Object} task
  */
-const assignedTaskToProvider = async (task) => {
-  // 1. Search for online task providers
-  const onlineUsers = await User.find({
+const assignedTaskToProvider = handleAsync(async (task) => {
+  // 1. Search for online service providers
+  const providers = await User.find({
     online: true,
     role: eUserRole.PROVIDER,
     services: { $in: [task.service] },
@@ -91,22 +90,20 @@ const assignedTaskToProvider = async (task) => {
     },
   });
 
-  // 2. Check if any provider is available
-  if (onlineUsers.length === CONST_ZEROU) {
-    io.emit('error:no-provider', { message: 'No provider available' });
-    return;
+  // 2. Assign the task to a provider if found
+  if (providers.length > CONST_ZEROU) {
+    task.provider = providers[CONST_ZEROU].id;
+    task.status = eTaskStatus.ASSIGNED;
+
+    // 3. Save updated task
+    await task.save();
+
+    // 4. Emit message
+    io.emit('task:assigned', { message: 'Task assigned!', data: task });
+  } else {
+    io.emit('task:no-provider', { message: 'No provider found yet!' });
   }
-
-  // 3. Assigned task to provider who is online
-  const closestUser = onlineUsers[CONST_ZEROU];
-
-  // 4. Update task provider and status
-  task.provider = closestUser._id;
-  task.status = eTaskStatus.ASSIGNED;
-
-  // 5. Save updated task
-  await task.save();
-};
+});
 
 /**
  * @breif Create a new task
@@ -116,7 +113,7 @@ const createTask = catchAsync(async (req, res, next) => {
   const newTask = await Task.create(req.body);
 
   // 2. Assign task to provider
-  await assignedTaskToProvider(newTask);
+  assignedTaskToProvider(newTask)(next);
 
   // 3. Send back response to user
   res.status(201).json({
@@ -133,6 +130,7 @@ const toggleTaskStatus = catchAsync(async (req, res, next) => {
   // 1. Get the task
   const task = await Task.findById(req.params.taskId);
 
+  // 2. Check if task exist
   if (!task) {
     return next(
       new AppError(
@@ -142,36 +140,57 @@ const toggleTaskStatus = catchAsync(async (req, res, next) => {
     );
   }
 
-  const { status } = req.body;
+  const { status } = req.body; // get new status
   task.status = status;
-  // Provider rejects task
-  if (status === eTaskStatus.REJECTED) {
-    if (!task.prevProviders.includes(req.user._id)) {
-      task.prevProviders.push(req.user.id);
-    }
-    task.provider = null;
-    io.emit('task:rejected', {
-      data: task,
-      message: `Provider ${req.user.firstname} rejects task`,
-    });
+
+  // 3. Update task base on status
+  switch (status) {
+    // Provider rejects task
+    case status === eTaskStatus.REJECTED:
+      if (!task.prevProviders.includes(req.user._id)) {
+        task.prevProviders.push(req.user.id);
+      }
+      task.provider = null;
+      io.emit('task:rejected', {
+        data: task,
+        message: 'Provider rejects task',
+      });
+      break;
+    // Provider accepts task
+    case status === eTaskStatus.ACCEPTED:
+      const providerExist =
+        task.prevProviders && task.prevProviders.indexOf(req.user._id);
+
+      if (providerExist && providerExist >= CONST_ZEROU) {
+        task.prevProviders.splice(providerExist, CONST_ONEU);
+      }
+      task.provider = req.user._id;
+
+      io.emit('task:accepted', {
+        data: task,
+        message: 'Provider accepts to do task',
+      });
+      break;
+    // Provider starts task
+    case status === eTaskStatus.PROGRESS:
+      io.emit('task:in-progress', { data: task, message: 'Task in progress!' });
+      break;
+    // Provider completes task
+    case status === eTaskStatus.COMPLETED:
+      io.emit('task:completed', { data: task, message: 'Task completed!' });
+      break;
+    // User approves task
+    case status === eTaskStatus.APPROVED:
+      io.emit('task:approved', { data: task, message: 'Task completed!' });
+      break;
+    default:
+      await task.save();
   }
 
-  // Provider accepts task
-  if (status === eTaskStatus.ACCEPTED) {
-    const providerExist =
-      task.prevProviders && task.prevProviders.indexOf(req.user._id);
-
-    if (providerExist && providerExist >= CONST_ZEROU) {
-      console.log(providerExist);
-      task.prevProviders.splice(providerExist, CONST_ONEU);
-    }
-    task.provider = req.user._id;
-
-    io.emit('task:accepted', { data: task });
-  }
-
+  // 4. Save new task status
   await task.save();
 
+  // 5. Send response
   res.status(200).json({
     status: 'success',
     data: task,
