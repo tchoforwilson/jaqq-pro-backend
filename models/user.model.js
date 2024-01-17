5f97b6520f06c387a08d7199c70d48132e8c3958
tchoforwilson <tchoforwilson@gmail.com>
2024-01-17 05:46:09 +0100

    added ratings for providers field

--- models/user.model.js
+++ models/user.model.js
@@ -51,6 +51,17 @@ const userSchema = new Schema(
         ref: 'Service',
       },
     ],
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10, // 4.666666, 46.6666, 47, 4.7
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
     lastVerificationSMSCode: Number,
     smsCodeExpiresAt: Date,
     active: {
