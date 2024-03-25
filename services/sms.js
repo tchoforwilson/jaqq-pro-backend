c959cae07749d01c23d58d46685e3fd28ec704ca
tchoforwilson <tchoforwilson@gmail.com>
2024-03-25 03:12:43 +0100

    Refactored

--- services/sms.js
+++ services/sms.js
@@ -1,5 +1,5 @@
-import twilio from "twilio";
-import config from "../configurations/config.js";
+import twilio from 'twilio';
+import config from '../configurations/config.js';
 
 const client = twilio(config.twilio.sid, config.twilio.token);
 
@@ -9,7 +9,7 @@ const client = twilio(config.twilio.sid, config.twilio.token);
  * @param {String} to -> The receiver of the sms number
  */
 const sendMessage = async (message, to) => {
-  if (config.env === "production" || config.env === "development") {
+  if (config.env === 'production' || config.env === 'development') {
     await client.messages.create({
       from: config.twilio.from,
       body: message,
