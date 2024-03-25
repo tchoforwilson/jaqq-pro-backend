14c443dfe1d249dc9984928b25fdd4ee23b1ad07
tchoforwilson <tchoforwilson@gmail.com>
2024-03-25 03:11:56 +0100

    expo push notifications

---
+++ utilities/pushNotifications.js
@@ -0,0 +1,35 @@
+import { Expo } from 'expo-server-sdk';
+
+/**
+ * @breif Send a push notification to the user
+ * @param {String} targetExpoPushToken Expo Push notification token
+ * @param {Object} message Message to send
+ */
+const sendPushNotification = async (targetExpoPushToken, message) => {
+  const expo = new Expo();
+
+  const chunks = expo.chunkPushNotifications([
+    {
+      to: targetExpoPushToken,
+      sound: 'default',
+      body: message,
+    },
+  ]);
+
+  const sendChunks = async () => {
+    chunks.forEach(async (chunk) => {
+      console.log('Sending Chunk', chunk);
+
+      try {
+        const tickets = await expo.sendPushNotificationsAsync(chunk);
+        console.log('Tickets', tickets);
+      } catch (error) {
+        console.log('Error sending chunk', error);
+      }
+    });
+  };
+
+  await sendChunks();
+};
+
+export default sendPushNotification;
