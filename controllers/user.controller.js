5ca341d192dfea959213b3ab79d1363a9e89a641
tchoforwilson <tchoforwilson@gmail.com>
2024-03-26 05:04:34 +0100

    Update photo name path

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,17 +1,17 @@
-import sharp from "sharp";
-import User from "../models/user.model.js";
+import sharp from 'sharp';
+import User from '../models/user.model.js';
 
-import factory from "./handler.factory.js";
-import AppError from "../utilities/appError.js";
-import catchAsync from "../utilities/catchAsync.js";
-import { filterObj } from "../utilities/utils.js";
-import { upload } from "../utilities/upload.js";
-import eStatusCode from "../utilities/enums/e.status-code.js";
+import factory from './handler.factory.js';
+import AppError from '../utilities/appError.js';
+import catchAsync from '../utilities/catchAsync.js';
+import { filterObj } from '../utilities/utils.js';
+import { upload } from '../utilities/upload.js';
+import eStatusCode from '../utilities/enums/e.status-code.js';
 
 /**
  * @breif Upload a single user photo
  */
-const uploadUserPhoto = upload.single("photo");
+const uploadUserPhoto = upload.single('photo');
 
 /**
  * @breif Resize user photo to size 500x500 and convert format to jpeg
@@ -22,15 +22,17 @@ const resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
   // 2. Rename file
+  const imagePath = 'public/images/users';
+  const fullPath = `${req.protocol}://${req.get('host')}/${imagePath}`;
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
-  req.body.photo = req.file.filename;
+  req.body.photo = `${fullPath}/${req.file.filename}`;
 
   // 3. Upload file
   await sharp(req.file.buffer)
     .resize(500, 500)
-    .toFormat("jpeg")
+    .toFormat('jpeg')
     .jpeg({ quality: 90 })
-    .toFile(`public/images/users/${req.file.filename}`);
+    .toFile(`${imagePath}/${req.file.filename}`);
 
   next();
 });
@@ -57,13 +59,13 @@ const toggleMyServices = catchAsync(async (req, res, next) => {
 
   // 2. Check if the task are updated or user not found
   if (!user) {
-    return next(new AppError(eStatusCode.NOT_FOUND, "User not found!"));
+    return next(new AppError(eStatusCode.NOT_FOUND, 'User not found!'));
   }
 
   // 3. Send response
   res.status(eStatusCode.SUCCESS).json({
-    status: "success",
-    message: "Services updated successfully",
+    status: 'success',
+    message: 'Services updated successfully',
     data: user,
   });
 });
@@ -76,7 +78,7 @@ const updateMe = catchAsync(async (req, res, next) => {
   if (req.body.password || req.body.passwordConfirm) {
     return next(
       new AppError(
-        "This route is not for password updates. Please use /updateMyPassword.",
+        'This route is not for password updates. Please use /updateMyPassword.',
         eStatusCode.BAD_REQUEST
       )
     );
@@ -85,13 +87,13 @@ const updateMe = catchAsync(async (req, res, next) => {
   // 2) Filtered out unwanted fields names that are not allowed to be updated
   const filteredBody = filterObj(
     req.body,
-    "firstname",
-    "lastname",
-    "birthday",
-    "email",
-    "services",
-    "photo",
-    "location"
+    'firstname',
+    'lastname',
+    'birthday',
+    'email',
+    'services',
+    'photo',
+    'location'
   );
 
   // 3) Update user document
@@ -101,11 +103,52 @@ const updateMe = catchAsync(async (req, res, next) => {
   });
 
   res.status(eStatusCode.SUCCESS).json({
-    status: "success",
-    message: "data updated!",
+    status: 'success',
+    message: 'data updated!',
     data: {
       user: updatedUser,
     },
+  });
+});
+
+/**
+ * @breif Update user push notification token
+ */
+const updatePushToken = catchAsync(async (req, res, next) => {
+  // 1. Get user
+  const user = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      pushToken: req.body.pushToken,
+    },
+    { new: true, runValidators: false }
+  );
+
+  // 2. Check if  the task are updated or user not found
+  if (!user) {
+    return next(new AppError(eStatusCode.NOT_FOUND, 'User not found!'));
+  }
+
+  // 3. Send response
+  res.status(eStatusCode.SUCCESS).json({
+    status: 'success',
+    message: 'Push token updated successfully',
+    data: user,
+  });
+});
+
+/**
+ * @breif Fetch all current user services
+ */
+const getUserServices = catchAsync(async (req, res, next) => {
+  // 1. Get all user services
+  const services = (await User.findById(req.user._id)).services;
+
+  // 2. Send response
+  res.status(eStatusCode.SUCCESS).json({
+    status: 'success',
+    message: 'Services successfully retrived!',
+    data: services,
   });
 });
 
@@ -120,15 +163,15 @@ const deleteMe = catchAsync(async (req, res, next) => {
 
   // 2. Send response
   res.status(eStatusCode.NO_CONTENT).json({
-    status: "success",
+    status: 'success',
     data: null,
   });
 });
 
 const createUser = (req, res) => {
   res.status(eStatusCode.SERVER_ERROR).json({
-    status: "error",
-    message: "This route is not defined! Please use /signup instead",
+    status: 'error',
+    message: 'This route is not defined! Please use /signup instead',
   });
 };
 
@@ -146,6 +189,8 @@ export default {
   resizeUserPhoto,
   getMe,
   updateMe,
+  updatePushToken,
+  getUserServices,
   toggleMyServices,
   deleteMe,
   createUser,
