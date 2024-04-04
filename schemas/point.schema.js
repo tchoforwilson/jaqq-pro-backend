15a43533ada1f5770e1db7107bfab781da30be8b
tchoforwilson <tchoforwilson@gmail.com>
2024-04-04 19:49:07 +0100

    Added name and category field

--- schemas/point.schema.js
+++ schemas/point.schema.js
@@ -1,4 +1,4 @@
-import { Schema } from "mongoose";
+import { Schema } from 'mongoose';
 
 /**
  * @breif Coordinates point schema
@@ -6,11 +6,13 @@ import { Schema } from "mongoose";
 const pointSchema = new Schema({
   type: {
     type: String,
-    enum: ["Point"],
+    enum: ['Point'],
   },
   coordinates: {
     type: [Number],
   },
+  name: String,
+  category: String,
 });
 
 export default pointSchema;
