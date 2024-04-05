40a305bb02f6950b3d5f0c4213dd3431528f2fa9
tchoforwilson <tchoforwilson@gmail.com>
2024-04-05 09:25:54 +0100

    Connection actions

--- actions/connection.js
+++ actions/connection.js
@@ -1,74 +1,7 @@
-import { promisify } from 'es6-promisify';
-import jwt from 'jsonwebtoken';
-import User from '../models/user.model.js';
-import SocketError from '../utilities/socketError.js';
-import config from '../configurations/config.js';
+import userHandler from './user.socket.js';
 
 const onConnection = async (socket) => {
-  // 1. Get token from header
-  let token;
-  if (socket.handshake.headers.token) {
-    token = socket.handshake.headers.token;
-  }
-
-  // 2. check if token
-  if (!token) return SocketError(socket, 'You are not allowed to login!');
-
-  // 3. Verify token
-  const decoded = await promisify(jwt.verify)(token, config.jwt.secret);
-
-  // 4. Check if user still exists
-  const currentuser = await User.findById(decoded.id);
-
-  if (!currentuser)
-    return SocketError(
-      socket,
-      'The user belonging to this token no longer exists!'
-    );
-
-  // 5. Check if user changed password after the token was issued
-  if (currentuser.changedPasswordAfter(decoded.iat)) {
-    return SocketError(
-      socket,
-      'User received changed password! Please log in again,'
-    );
-  }
-  // 6. Update user connection
-  currentuser.online = true;
-  currentuser.lastConnection = Date.now();
-  currentuser.connectionId = socket.id;
-  const updatedUser = await currentuser.save({ validateModifiedOnly: true });
-
-  // 7. Emit user:connected event
-  socket.emit('connected', { data: updatedUser });
-
-  /**
-   * @bref Current user location
-   */
-  socket.on('currentLocation', async (location) => {
-    currentuser.currentLocation = location;
-    await currentuser.save({ validateBeforeSave: false });
-    socket.emit('currentLocation:updated', {
-      message: 'Current location updated!',
-      data: currentuser,
-    });
-  });
-
-  /**
-   * @breif When user disconnects from server
-   */
-  socket.on('disconnect', async () => {
-    await User.findOneAndUpdate(
-      { connectionId: socket.id },
-      {
-        online: false,
-        lastConnection: Date.now(),
-        connectionId: null,
-      }
-    );
-
-    socket.emit('disconnected', { data: currentuser });
-  });
+  userHandler(socket);
 };
 
 export default onConnection;
