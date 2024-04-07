9a5fa8b72568ecba1176be76d9cac81242c9d1ba
tchoforwilson <tchoforwilson@gmail.com>
2024-04-07 03:44:44 +0100

    Change max distance to 5 meters

--- utilities/constants/index.js
+++ utilities/constants/index.js
@@ -1,4 +1,4 @@
 export const SMS_LENGTH = Number(4);
 export const CONST_ZEROU = Number(0);
 export const CONST_ONEU = Number(1);
-export const MAX_PROVIDER_DISTANCE = Number(10000);
+export const MAX_PROVIDER_DISTANCE = Number(50000);
