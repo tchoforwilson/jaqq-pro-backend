a7ba794a85e6fa89fe0445e8761a6fe7071d6587
tchoforwilson <tchoforwilson@gmail.com>
2024-06-21 06:56:40 +0100

    unused variables

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -5,15 +5,11 @@ import User from '../models/user.model.js';
 import Task from '../models/task.model.js';
 import eUserRole from '../utilities/enums/e.user-role.js';
 import eTaskStatus from '../utilities/enums/e.task-status.js';
-import handleAsync from '../utilities/errors/handleAsync.js';
 import catchAsync from '../utilities/catchAsync.js';
-import AppError from '../utilities/appError.js';
 import {
-  CONST_ONEU,
   CONST_ZEROU,
   MAX_PROVIDER_DISTANCE,
 } from '../utilities/constants/index.js';
-import eStatusCode from '../utilities/enums/e.status-code.js';
 
 /**
  * @breif Set task user id
@@ -157,201 +153,6 @@ const createTask = catchAsync(async (req, res, next) => {
   });
 });
 
-const checkIfTaskExists = catchAsync(async (req, res, next) => {
-  // 1. Get the task
-  const task = await Task.findById(req.params.id);
-  // 2. Check task exists
-  if (!task) return next(new AppError('Task not found', eStatusCode.NOT_FOUND));
-  // 3. next
-  req.task = task;
-  next();
-});
-
-const setInProgress = catchAsync(async (req, res, next) => {
-  // 1. Get task
-  const task = req.task;
-
-  // 2. Check if user location and task are same
-  const result = await task.atTaskLocation(req.user.currentLocation);
-  if (!result) {
-    return next(
-      new AppError('Your are not yet close to task', eStatusCode.BAD_REQUEST)
-    );
-  }
-
-  // 3. Update task by setting to in progess
-  const updatedTask = await Task.findByIdAndUpdate(
-    task._id,
-    {
-      status: eTaskStatus.PROGRESS,
-    },
-    {
-      new: true,
-    }
-  );
-
-  // 4. Emit to user
-  io.emit('task:in-progress', {
-    message: 'Task set to in progess',
-    data: updatedTask,
-  });
-
-  res.status(200).json({
-    status: 'success',
-    message: 'Task updated successfully to in progress',
-    data: updatedTask,
-  });
-});
-
-/**
- * @desc Set task to ready for execution
- * @route /tasks/:id/ready
- * @access Private only to providers
- */
-const setTaskReady = catchAsync(async (req, res, next) => {
-  // 1. Get task
-  const task = req.task;
-
-  // 2. Update task status
-  const updatedTask = await Task.findByIdAndUpdate(
-    task._id,
-    {
-      status: eTaskStatus.READY,
-    },
-    {
-      new: true,
-    }
-  );
-
-  // 3. Send response
-  // a. To user
-  io.emit('task:ready', { data: updatedTask, message: 'Task is ready!' });
-  // b.To Provider
-  res.status(200).json({
-    status: 'success',
-    message: 'Task set to ready!',
-    data: updatedTask,
-  });
-});
-
-const setTaskApproved = catchAsync(async (req, res, next) => {
-  // 1. Get task
-  const task = req.task;
-
-  // 2. Update task status
-  const updatedTask = await Task.findByIdAndUpdate(
-    task._id,
-    {
-      status: eTaskStatus.APPROVED,
-    },
-    {
-      new: true,
-    }
-  );
-
-  // 4. Move payment to user account
-
-  // 4. Send response
-  // a. To user
-  io.emit('task:approved', { data: updatedTask, message: 'Task approved!' });
-  // b.To Provider
-  res.status(200).json({
-    status: 'success',
-    message: 'Task is approved!',
-    data: updatedTask,
-  });
-});
-
-const setTaskCancell = catchAsync(async (req, res, next) => {
-  // 1. Get task
-  const task = req.task;
-
-  // 2. Check if task is in progress
-  if (task.status === eTaskStatus.PROGRESS) {
-    return next(
-      new AppError(
-        'Task is already in progress, cannot be cancelled',
-        eStatusCode.BAD_REQUEST
-      )
-    );
-  }
-
-  // 3. Update task status
-  const updatedTask = await Task.findByIdAndUpdate(task._id, {
-    status: eTaskStatus.CANCELLED,
-  });
-
-  // 4. Deduce from customer account
-
-  // 4. Send response
-  // a. To user
-  io.emit('task:cancelled', { data: updatedTask, message: 'Task cancelled!' });
-
-  // b.To Provider
-  res.status(200).json({
-    status: 'success',
-    message: 'Task is cancelled!',
-    data: updatedTask,
-  });
-});
-
-/**
- * @breif Change task status
- */
-const toggleTaskStatus = catchAsync(async (req, res, next) => {
-  // 1. Get task
-  const task = req.task;
-
-  const { status } = req.body; // get new status
-  task.status = status;
-
-  // 3. Update task base on status
-  switch (status) {
-    // Provider rejects task
-    case status === eTaskStatus.REJECTED:
-      if (!task.prevProviders.includes(req.user._id)) {
-        task.prevProviders.push(req.user.id);
-      }
-      task.provider = null;
-      io.emit('task:rejected', {
-        data: task,
-        message: 'Provider rejects task',
-      });
-      break;
-    // Provider accepts task
-    case status === eTaskStatus.ACCEPTED:
-      const providerExist =
-        task.prevProviders && task.prevProviders.indexOf(req.user._id);
-
-      if (providerExist && providerExist >= CONST_ZEROU) {
-        task.prevProviders.splice(providerExist, CONST_ONEU);
-      }
-      task.provider = req.user._id;
-
-      io.emit('task:accepted', {
-        data: task,
-        message: 'Provider accepts to do task',
-      });
-      break;
-    // Provider starts task
-    case status === eTaskStatus.PROGRESS:
-      io.emit('task:in-progress', { data: task, message: 'Task in progress!' });
-      break;
-    default:
-      await task.save();
-  }
-
-  // 4. Save new task status
-  await task.save();
-
-  // 5. Send response
-  res.status(200).json({
-    status: 'success',
-    message: `Task status changed to ${task.status}`,
-    data: task,
-  });
-});
-
 /**
  * @breif Delete task from current task list
  */
@@ -378,13 +179,7 @@ const deleteTask = catchAsync(async (req, res, next) => {
 
 export default {
   setTaskUserId,
-  checkIfTaskExists,
   createTask,
-  setInProgress,
-  setTaskReady,
-  setTaskApproved,
-  setTaskCancell,
-  toggleTaskStatus,
   getAllTasks: factory.getAll(Task),
   getTask: factory.getOne(Task),
   updateTask: factory.updateOne(Task),
